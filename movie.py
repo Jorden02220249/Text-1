98ea1991306f9ed213c0ba6c2ed384d9d723b82d
Jorden02220249 <02220249.cst@rub.edu.bt>
2024-02-28 06:52:47 -0800

    movie.py

---
+++ movie.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age: "))
+is_student = input("Are you a student? (yes/no): ")
+
+# Determine discount eligibility
+is_eligible = (age <= 12) or ((13 <= age <= 18) and (is_student.lower() == "yes"))
+
+# Print discount eligibility message
+if is_eligible:
+    print("You are eligible for a discount.")
+else:
+    print("You are not eligible for a discount.")
